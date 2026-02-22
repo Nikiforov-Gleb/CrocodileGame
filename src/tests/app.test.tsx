aa840019babb53bda1813ad76052b686ec653beb
Nikiforov-Gleb <gl.nikiforov@gmail.com>
2026-02-22 20:16:11 +0300

    Lint:fix

--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "../App";
 
-describe('App component', () => {
-  test('renders Hello world text', () => {
+describe("App component", () => {
+  test("renders Hello world text", () => {
     render(<App />);
     const element = screen.getByText(/hello world/i);
     expect(element).toBeInTheDocument();
