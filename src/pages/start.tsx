68e3a81f037ad9bb1c2e0f0f18661337225b8628
Nikiforov-Gleb <gl.nikiforov@gmail.com>
2026-03-21 16:07:57 +0300

    Lint fix

--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -48,7 +48,11 @@ export const StartPage: FC = () => {
             ))}
           </select>
         </div>
-        <button className="start" onClick={handleStartGame} disabled={!nameInput.trim()}>
+        <button
+          className="start"
+          onClick={handleStartGame}
+          disabled={!nameInput.trim()}
+        >
           Начать игру
         </button>
       </div>
